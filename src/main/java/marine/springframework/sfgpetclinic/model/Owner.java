eede9b91f1afd39ee78700a861f047b6592c1975
MinhTri1401 <minhtri1412000@gmail.com>
2020-08-24 14:46:19 +0700

    adding POJOs. closes #2

---
+++ src/main/java/marine/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package marine.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/marine/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package marine.springframework.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
